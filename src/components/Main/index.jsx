0d671b350eb849dcb9e657f8d37e919a30771ce0
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2024-12-24 20:03:20 -0300

    Defining the central image of the project

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,13 @@
+import styles from './Main.module.css'
+
+const Main = () => {
+  return (
+    <main className={styles.image}>
+        <div className={styles.main_image}>
+            <h2>Auto Espeto Butiquim</h2>
+        </div>
+    </main>
+  )
+};
+
+export default Main;
